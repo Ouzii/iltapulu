4e89b55613c5def2abfccf0a6e33cf440f4d6f1b
Ouzii <oskari.laaja@gmail.com>
2017-12-07 14:28:50 +0200

    Muutettu aikaformaattia

--- src/main/java/laajaosk/wepa/domain/News.java
+++ src/main/java/laajaosk/wepa/domain/News.java
@@ -3,10 +3,9 @@ package laajaosk.wepa.domain;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -26,7 +25,7 @@ public class News extends AbstractPersistable<Long> {
     private List<Category> categories = new ArrayList<>();
     private String text;
     private String ingress;
-    private Date published = new Date();
+    private String published = new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(2)).toString();
     
     @ManyToMany
     private List<Writer> writers = new ArrayList<>();
